417ced051b6c5908dc15607affb3540eafba41cc
root <root@localhost>
2016-10-17 18:56:57 +0300

    0

--- karekok.c
+++ karekok.c
@@ -1,24 +1,30 @@
 //Bir sayının karekökü nasıl hesaplanır 
 #include<stdio.h>
 int main (){
-	float sayi=2,kok=1;
+	float sayi=12,kok=1;
+	int sayac=0;
 	float katsayi=100;
 	int devam=1;
 
 	while(devam){
+  	 if(katsayi==1/100){
+				devam=0;
+				printf("kok:%f",kok);
+				}
 		
-		if(kok*kok<sayi) kok+=katsayi;
+		if(kok*kok<sayi) {
+			kok+=katsayi;
+			}
 		if(kok*kok>sayi){
 			kok-=katsayi;
 			katsayi=katsayi/10;
+		
 		}
 		if(sayi==kok*kok){
 			devam=0;
 			printf("%f'nin karekökü: %f",sayi,kok);
 			}
-			else if(katsayi==1/10){
-				devam=0;
-				printf("kok:%f",kok);}
+
 		}
 	
 	return 0;
