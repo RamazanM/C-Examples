baa4f307d77b9cc78bec008d5d75ef11dd600653
root <root@localhost>
2016-10-22 12:33:48 +0300

    basit logaritma fonksiyonu yazıldı

--- logarithm.c
+++ logarithm.c
@@ -5,11 +5,15 @@ int main (){
 	scanf ("%d",&taban);
 	printf ("Sayi:");
 	scanf ("%d",&sayi);
-	int i=0;
+	int i=1;
 	while (1){
-		if (sayi!=taban*i)i++;
+		if (sayi!=pow (taban,i))i++;
 		else break;
 	}
-	printf ("%d",i);
+	printf ("log%d tabanında %d = %d",taban,sayi,i);
 }
-int pow (int a, int b){}
+int pow (int a, int b){
+	int i=0,sonuc=1;
+	for (i;i<b;i++) sonuc*=a;
+	return sonuc;
+	}
