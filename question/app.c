bc2da09e39dad169b120ead4ebff74a4a8f535cb
RamazanM <r.ramazanmutlu@gmail.com>
2016-11-04 18:11:57 +0200

    Update

--- question/app.c
+++ question/app.c
@@ -14,8 +14,11 @@ int setup();
 int soru_cek();
 int rastgele_soru();
 int soru_sayisi();
-int rastgele(int max);
+int rastgele(int);
+int cevap_to_sayi(char);
+int getch(void);
 void game();
+void gameover();
 
 struct soru {
   int soru_id;
@@ -30,43 +33,58 @@ struct oyun{
   char *soru_dosyasi;
   int *sorulanlar;
   int sorulan_sayisi;
+  int dogru_sayisi;
+  int yanlis_sayisi;
   int puan;
 }oyun;
 
 int main(){
   setup();
-  while (1) {
-    game();
-  }
+  while (oyun.yanlis_sayisi==0) game();
+  gameover();
 }
 
-void game(){
-  int cevap;
+void game(){          //oyunun ana mekanizmasını oluşturur.
+  char cevap=-1;
   if(soru_cek()){
     printf("%d)%s\nA)%s\tB)%s\tC)%s\tD)%s\n",
     oyun.sorulan_sayisi,
     soru.soru_metni,
-    soru.cevaplar[0],soru.cevaplar[1],soru.cevaplar[2],soru.cevaplar[3]);
-    scanf("%d",&cevap);
-    if(cevap==soru.dogru)printf("Doğru cevap\n");
-    else printf("Yanlış cevap\n");
+    soru.cevaplar[0],
+    soru.cevaplar[1],
+    soru.cevaplar[2],
+    soru.cevaplar[3]);
+
+    while(cevap_to_sayi(cevap)==-1){
+      printf("\nCevap:");
+      cevap=getch();
+      printf("%c\t",cevap);
+    }
+      if(cevap_to_sayi(cevap)==soru.dogru){
+        printf("Doğru cevap\n\n");
+        oyun.dogru_sayisi++;
+      }
+      else oyun.yanlis_sayisi++;
+
   }
 }
 
-int setup(){
-  printf("Hoşgeldiniz. Cevapların altındaki bar dolmadan doğru cevabı vermeniz gerekmekte.\n");
+int setup(){              //oyunun çalışması için gerekli ayarlamaları yapar.
+  printf("Hoşgeldiniz. Cevapların altındaki bar dolmadan doğru cevabı vermeniz gerekmekte.\n\n");
   int i=0;
   oyun.soru_dosyasi="./sorular.bin";
   oyun.sorulan_sayisi=0;
   oyun.puan=0;
-  oyun.sorulanlar=malloc(50*sizeof(int));
+  oyun.dogru_sayisi=0;
+  oyun.yanlis_sayisi=0;
+  oyun.sorulanlar=calloc(50,sizeof(int));
   soru.cevaplar=malloc(4*sizeof(int));
   soru.soru_metni=malloc(250*sizeof(char));
   for(i;i<4;i++)soru.cevaplar[i]=malloc(50*sizeof(char));
 
 }
 
-int soru_cek(){         //rastgele soru çekip, soru nesnesini doldurur. hata varsa 0 döndürür.
+int soru_cek(){           //rastgele soru çekip, soru nesnesini doldurur. hata varsa 0 döndürür.
   FILE *dosya;
   char *line=NULL;
   char *donen_satir=NULL;
@@ -79,7 +97,7 @@ int soru_cek(){         //rastgele soru çekip, soru nesnesini doldurur. hata va
   if (dosya!=NULL) {
     int sayac=0;
     while ((oku=getline(&line, &len,dosya))!= -1) {
-      if (sayac==secili_satir) {
+      if (sayac==secili_satir-1) {
         donen_satir=line;
         break;
       }
@@ -95,38 +113,30 @@ int soru_cek(){         //rastgele soru çekip, soru nesnesini doldurur. hata va
     &soru.dogru);
     oyun.sorulanlar[oyun.sorulan_sayisi]=soru.soru_id;
     oyun.sorulan_sayisi++;
-    printf("soru:%d\n",soru.soru_id );
-    printf("soru_c:%d,%d\n",oyun.sorulanlar[0],oyun.sorulanlar[1] );
     return 1;
   }
   else return 0;
 
 }
 
-int rastgele_soru(){
-  int temp=rastgele(soru_sayisi());
-  int i=0,devam;
-  //printf("Rastgele soru:%d\n",temp);
-
-  while(1){
-    devam=0;
-
-    for(i=0;i<=oyun.sorulan_sayisi;i++){
-      if(oyun.sorulanlar[i]==temp){
-        temp=rastgele(soru_sayisi());
-        devam=1;
+int rastgele_soru(){      //soru_cek fonksiyonu için rastgele soru sağlar.
+  int flag=1,temp,i;
+  int sorulan_sayisi=oyun.sorulan_sayisi;
+  while (flag){
+    flag=0;
+    temp=rastgele(soru_sayisi()+1);
+    for(i=0;i<sorulan_sayisi;i++){
+      //printf("temp=%d\toyun.sorulanlar[%d]=%d\n",temp,i,oyun.sorulanlar[i]);
+      if(oyun.sorulanlar[i]==temp) {
+        i=sorulan_sayisi;
+        flag=1;
       }
     }
-
-    if(devam==0){
-      return temp;
-      //break;
-    }
   }
-
+  return temp;
 }
 
-int soru_sayisi(){
+int soru_sayisi(){        //soru dosyasındaki soru sayısını döndürür.
   FILE *dosya;
   char *line=NULL;
   size_t len=0;
@@ -143,6 +153,32 @@ int rastgele(int max){    //rastgele sayı üretir.
   while (sayi==0) {
     sayi=rand()%max;
   }
+  sleep(1);
   srand(time(NULL));
   return sayi;
+}
+
+int getch(void){          //http://stackoverflow.com/a/23035044
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+
+int cevap_to_sayi(char c){//harf olarak gelen cevabı sayısal değere çevirir. hata:-1
+  if(c=='a'||c=='A'||c==0) return 0;
+  if(c=='b'||c=='B'||c==1) return 1;
+  if(c=='c'||c=='C'||c==2) return 2;
+  if(c=='d'||c=='D'||c==3) return 3;
+  else return -1;
+}
+
+void gameover(){
+  printf("Yanlış cevap verdiniz oyun bitti.\n");
+  printf("Puanınız:%d\n",oyun.dogru_sayisi);
 }
