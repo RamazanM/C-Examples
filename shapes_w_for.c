41d33e2d718bfbbba2c94052e66817c8eff8db87
root <root@localhost>
2016-11-17 11:42:58 +0300

    üçgenler eklendi

---
+++ shapes_w_for.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+void dik_ucgen();
+void bos_dik_ucgen();
+void bos_ikizkenar();
+void ikizkenar();
+
+int main (){
+	dik_ucgen();
+	printf("\n");
+	bos_dik_ucgen();
+	printf("\n");
+	bos_ikizkenar();
+	printf("\n");
+	ikizkenar();
+}
+
+void dik_ucgen(){
+	int i,j;
+	for(i=1;i <10;i++){
+		for (j=0;j <i;j++){
+			printf ("* ");
+		}
+		printf ("\n");
+	}	
+}
+
+void bos_dik_ucgen(){
+	int i,j;
+	for(i=0;i <=10;i++){
+		for (j=0;j <=i;j++){
+			if(i==10) printf("* ");
+			else if(j==0 || j==i) printf ("* ");
+			else printf("  ");
+		}
+		printf ("\n");
+	}
+}
+
+void ikizkenar(){
+	int i,j;
+	for(i=0;i<=7;i++){
+		for(j=0;j<15;j++){
+			if (i==7) printf("*");
+			else if( j < 7-i || j > 7+i) printf(" ");
+			else printf("*");
+			}
+			printf("\n");
+		}
+}
+
+
+void bos_ikizkenar(){
+	int i,j;
+	for(i=0;i<=7;i++){
+		for(j=0;j<15;j++){
+			if (i==7) printf("*");
+			else if( j == 7-i || j == 7+i) printf("*");
+			else printf(" ");
+			}
+			printf("\n");
+		}
+	}
