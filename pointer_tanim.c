d468bf457026182e2265c3c1f838000b1d30ed87
root <root@localhost>
2016-12-07 21:48:41 +0300

    pointer...

---
+++ pointer_tanim.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int a=12; 	//a isimli değişken.
+	int * p;		   //p isimli pointer
+	p=&a; 		//p pointer'ı a'nın adresini gösterir artık;
+	
+	printf("a:%d\n",a);	//a'nın içeriği
+	printf("p:%x\n",p);	//p'nin içeriği(gösterdiği adres)
+	printf("&p:%x\n",&p);	//p'nin adresi
+	printf("*p:%d\n",*p);	//p'nin gösterdiği adresteki değer.
+	}
