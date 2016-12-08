39866891ea50e22d6919b785832e94e6f99c03bc
root <root@localhost>
2016-12-08 11:51:19 +0300

    Card game in c...

---
+++ cardGame.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Card{
+	char sym;
+	int num;
+} card;
+
+typedef struct Player{
+	card cards[13];
+	} player;
+
+int main(){
+	player Oyuncular[4];
+	int i;	
+	for( i=0 ; i<4 ; i++){	
+	}
+}
+
+printCards(Player oyuncu){
+	}
+
+shareCards(card * dizi, player oyuncu){
+	}
