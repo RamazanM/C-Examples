e04d0fea7968f5333ebe30ad8e27e2e81c526e9e
RamazanM <r.ramazanmutlu@gmail.com>
2017-01-15 15:44:58 +0300

    Update

--- dby-realloc.c
+++ dby-realloc.c
@@ -10,18 +10,20 @@ Ramazan Mutlu
 
 int main(){
     int *notlar,i=0,toplam=0,ort=0; //notları gösterecek pointer'ı ve gerekli değişkenleri tanımladık.
-    malloc(1*sizeof(int));  //1 integerlık yer ayırdık.
+    notlar=(int*)malloc(i*sizeof(int));  //1 integerlık yer ayırdık.
 
     while (1) { // Programı sonsuz döngüye soktuk, ileride break ile çıkacağız.
       printf("Not giriniz:");
-      scanf("%d",notlar+i); //notlar'ın i sonrasına gelen değeri atadık. notlar zaten bir pointer olduğu için & işareti kullanmadık.
+      scanf("%d",(notlar+i)); //notlar'ın i sonrasına gelen değeri atadık. notlar zaten bir pointer olduğu için & işareti kullanmadık.
 
       if( *(notlar+i) != -1 ){  //Girilen sayının -1 olup olmadığını kontrol ettik.
-        toplam+=*(notlar+i);  //toplam değişkenine girilen sayıyı ekledik.
         i++;  //i sayacımızı 1 arttırdık.
+        notlar =(int*) realloc(notlar,i*sizeof(int)); //Diziyi i eleman alacak şekilde genişlettik.
+        printf("%d\n",*(notlar+i-1) );
+        toplam+=*(notlar+i-1);  //toplam değişkenine girilen sayıyı ekledik.
       }
       else  break;  //girilen sayı -1 ise döngüden çık.
     }
-    ort=toplam/i;
+    ort=toplam/(i);
     printf("Notlarin toplami:%d\nOrtalamasi:%d\n",toplam,ort );
 }
