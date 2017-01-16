5591875acae67342f2a033aa693bef915afc9f0f
RamazanM <r.ramazanmutlu@gmail.com>
2017-01-16 08:45:51 +0300

    Realloc Düzenlendi.

--- dby-realloc.c
+++ dby-realloc.c
@@ -9,21 +9,24 @@ Ramazan Mutlu
 #include <stdlib.h> //Dinamik bellek yönetimi kodlarını yazabilmemiz için stdlib kütüphanesi gereklidir.
 
 int main(){
-    int *notlar,i=0,toplam=0,ort=0; //notları gösterecek pointer'ı ve gerekli değişkenleri tanımladık.
+    int *notlar,i=1,toplam=0,ort=0; //notları gösterecek pointer'ı ve gerekli değişkenleri tanımladık.
     notlar=(int*)malloc(i*sizeof(int));  //1 integerlık yer ayırdık.
 
     while (1) { // Programı sonsuz döngüye soktuk, ileride break ile çıkacağız.
       printf("Not giriniz:");
-      scanf("%d",(notlar+i)); //notlar'ın i sonrasına gelen değeri atadık. notlar zaten bir pointer olduğu için & işareti kullanmadık.
+      scanf("%d",(notlar+i-1)); //notlar'ın i sonrasına gelen değeri atadık. notlar zaten bir pointer olduğu için & işareti kullanmadık.
 
-      if( *(notlar+i) != -1 ){  //Girilen sayının -1 olup olmadığını kontrol ettik.
+      if( *(notlar+i-1) != -1 ){  //Girilen sayının -1 olup olmadığını kontrol ettik.
         i++;  //i sayacımızı 1 arttırdık.
         notlar =(int*) realloc(notlar,i*sizeof(int)); //Diziyi i eleman alacak şekilde genişlettik.
-        printf("%d\n",*(notlar+i-1) );
-        toplam+=*(notlar+i-1);  //toplam değişkenine girilen sayıyı ekledik.
+        printf("%d\n",*(notlar+i-2) );
+        toplam+=*(notlar+i-2);  //toplam değişkenine girilen sayıyı ekledik.
+        
       }
-      else  break;  //girilen sayı -1 ise döngüden çık.
+      else{  
+      	break;
+      }  //girilen sayı -1 ise döngüden çık.
     }
-    ort=toplam/(i);
+    ort=toplam/(i-1);
     printf("Notlarin toplami:%d\nOrtalamasi:%d\n",toplam,ort );
 }
--- pointer_arrays2.c
+++ pointer_arrays2.c
@@ -1,26 +1,21 @@
 #include<stdio.h>
-#include <time.h>
+#include<time.h>
 
 void randomDoldur (int * dizi , int length);
 void diziYazdir (int * dizi, int length);
- 
-
 
 int main (){
-
- srand (time (NULL));
-
- int sayilar[10];
- randomDoldur(sayilar,10);
- diziYazdir(sayilar,10);
+ srand (time (NULL));	//Rastgeleliliği sıfırla.
+ int sayilar[10];					//10 elemanlı bir sayı dizisi tanımla
+ randomDoldur(sayilar,10);//Sayılar dizisini rastgele doldur
+ diziYazdir(sayilar,10);//Diziyi ekrana yazdır
  
 }
 
 void randomDoldur (int * dizi , int length){
  int i;
  for (i=0;i <length;i++)
-  *(dizi+i)=rand()%51;
-  
+  *(dizi+i)=rand()%51;  
 }
 
 void diziYazdir (int * dizi, int length){
